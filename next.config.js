c6ec663e9c5f4340428b5c8f7d93e320cc8441d5
Drew Klayman <drewklay@gmail.com>
2021-12-02 11:20:15 -0800

    removing config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   webpack: (config) => {
-    const experiments = config.experiments || {};
-    config.experiments = {...experiments, asyncWebAssembly: true};
-    config.output.assetModuleFilename = `static/[hash][ext]`;
-    config.output.publicPath = `/_next/`;
-    config.module.rules.push({
-      test: /\.wasm/,
-      type: 'asset/resource',
-    })
     return config
   },
 }
